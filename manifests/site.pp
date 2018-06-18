779d474deb77fc2bb264eed89ba19dd4d191930a
cchomelab <40346112+cchomelab@users.noreply.github.com>
2018-06-18 12:41:44 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,1 +1,14 @@
 foo
+node default {
+  file {'/root/README':
+    ensure =>  file,
+  }
+}
+
+node 'puppetmaster.example.com'
+{
+  file {'/root/MASTER':
+    ensure =>  file,
+  }
+  notify{"The is seen!"
+}
